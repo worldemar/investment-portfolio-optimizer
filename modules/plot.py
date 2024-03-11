476082b70b09f5d00d90400a6e8bd367cd3488de
Vladimir Looze <woldemar@mimas.ru>
2024-03-11 16:33:57 +0300

    plot capital gains over time for edge case portfolios

--- modules/plot.py
+++ modules/plot.py
@@ -11,6 +11,40 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as pltlines
 
 
+def draw_portfolios_history(
+        portfolios_list: list,
+        title: str, xlabel: str, ylabel: str,
+        color_map: dict):
+    time_start = time.time()
+    plot_data = []
+    for portfolio in portfolios_list:
+        line_data = []
+        for year in sorted(portfolio.annual_capital.keys()):
+            line_data.append({
+                'x': year,
+                'y': portfolio.annual_capital[year] * 100 - 100,
+                'text': '\n'.join([
+                    f'By {year}: {portfolio.annual_capital[year]*100-100:+.0f}%',
+                    '--------',
+                    f'{portfolio.plot_title()}'
+                ]),
+                'color': portfolio.plot_color(color_map),
+                'size': 50 / portfolio.number_of_assets(),
+            })
+        plot_data.append(line_data)
+    draw_circles_with_tooltips(
+        circle_lines=plot_data,
+        xlabel=xlabel,
+        ylabel=ylabel,
+        title=title,
+        directory='result',
+        filename=f'{ylabel} - {xlabel}',
+        asset_color_map=color_map,
+        portfolio_legend=portfolios_list)
+    time_end = time.time()
+    print(f'--- Graph ready: history --- {time_end-time_start:.2f}s')
+
+
 # pylint: disable=too-many-arguments
 def draw_portfolios_statistics(
         portfolios_list: list,
@@ -20,32 +54,33 @@ def draw_portfolios_statistics(
     time_start = time.time()
     plot_data = []
     for portfolio in portfolios_list:
-        plot_data.append({
+        plot_data.append([{
             'x': f_x(portfolio),
             'y': f_y(portfolio),
             'text': portfolio.plot_tooltip(),
             'color': portfolio.plot_color(color_map),
             'size': 50 / portfolio.number_of_assets(),
-        })
+        }])
     draw_circles_with_tooltips(
-        circles=plot_data,
+        circle_lines=plot_data,
         xlabel=xlabel,
         ylabel=ylabel,
         title=title,
         directory='result',
         filename=f'{ylabel} - {xlabel}',
-        color_legend=color_map)
+        asset_color_map=color_map)
     time_end = time.time()
     print(f'--- Graph ready: {ylabel} - {xlabel} --- {time_end-time_start:.2f}s')
 
 
 # pylint: disable=too-many-arguments
 # pylint: disable=too-many-locals
+# pylint: disable=too-many-statements
 def draw_circles_with_tooltips(
-        circles=None,
+        circle_lines=None,
         xlabel=None, ylabel=None, title=None,
-        directory='.', filename='plot.svg',
-        color_legend=None):
+        directory='.', filename='plot',
+        asset_color_map=None, portfolio_legend=None):
 
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -55,12 +90,12 @@ def draw_circles_with_tooltips(
     _, axes = plt.subplots(figsize=(9, 6))
 
     padding_percent = 15
-    xlim_min = min(c['x'] for c in circles)
-    xlim_max = max(c['x'] for c in circles)
+    xlim_min = min(c['x'] for line in circle_lines for c in line)
+    xlim_max = max(c['x'] for line in circle_lines for c in line)
     xlim_min_padded = xlim_min - padding_percent * (xlim_max - xlim_min) / 100
     xlim_max_padded = xlim_max + padding_percent * (xlim_max - xlim_min) / 100
-    ylim_min = min(c['y'] for c in circles)
-    ylim_max = max(c['y'] for c in circles)
+    ylim_min = min(c['y'] for line in circle_lines for c in line)
+    ylim_max = max(c['y'] for line in circle_lines for c in line)
     ylim_min_padded = ylim_min - padding_percent * (ylim_max - ylim_min) / 100
     ylim_max_padded = ylim_max + padding_percent * (ylim_max - ylim_min) / 100
     axes.set_xlim(xlim_min_padded, xlim_max_padded)
@@ -73,24 +108,61 @@ def draw_circles_with_tooltips(
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
-    handles = [
-        pltlines.Line2D(
-            [0], [0],
-            marker='o',
-            label=label,
-            linewidth=0,
-            markerfacecolor=color,
-            markeredgecolor='black'
-        ) for label, color in color_legend.items()
-    ]
-    axes.legend(
-        handles=handles,
-        fontsize='6',
-        facecolor='white',
-        framealpha=1
-    ).set_zorder(1)
+    if asset_color_map:
+        if portfolio_legend:
+            handles = [
+                pltlines.Line2D(
+                    [0], [0],
+                    marker='o',
+                    label=portfolio.plot_title(),
+                    linewidth=0,
+                    markerfacecolor=portfolio.plot_color(asset_color_map),
+                    markeredgecolor='black'
+                ) for portfolio in portfolio_legend
+            ]
+            axes.legend(
+                handles=handles,
+                fontsize='6',
+                facecolor='white',
+                framealpha=1
+            ).set_zorder(1)
+        else:
+            handles = [
+                pltlines.Line2D(
+                    [0], [0],
+                    marker='o',
+                    label=label,
+                    linewidth=0,
+                    markerfacecolor=color,
+                    markeredgecolor='black'
+                ) for label, color in asset_color_map.items()
+            ]
+            axes.legend(
+                handles=handles,
+                fontsize='6',
+                facecolor='white',
+                framealpha=1
+            ).set_zorder(1)
 
-    for index, circle in enumerate(circles):
+    all_circles = [circle for line in circle_lines for circle in line]
+
+    for line in circle_lines:
+        if len(line) > 1:
+            prev_circle = line[0]
+            for circle in line[1:]:
+                axes.plot(
+                    [prev_circle['x'], circle['x']],
+                    [prev_circle['y'], circle['y']],
+                    color=(
+                        (prev_circle['color'][0] + circle['color'][0]) / 2,
+                        (prev_circle['color'][1] + circle['color'][1]) / 2,
+                        (prev_circle['color'][2] + circle['color'][2]) / 2
+                    ),
+                    zorder=1
+                )
+                prev_circle = circle
+
+    for index, circle in enumerate(all_circles):
         axes.scatter(
             x=circle['x'],
             y=circle['y'],
@@ -98,11 +170,13 @@ def draw_circles_with_tooltips(
             facecolor=circle['color'],
             edgecolor='black',
             linewidth=circle['size'] / 50,
-            gid=f'patch_{index:08d}'
+            gid=f'patch_{index:08d}',
+            zorder=2
         )
+
     plt.savefig(os.path.join(directory, filename + '.png'), format="png", dpi=300)
 
-    for index, circle in enumerate(circles):
+    for index, circle in enumerate(all_circles):
         axes.annotate(
             gid=f'tooltip_{index:08d}',
             text=circle['text'],
@@ -117,7 +191,7 @@ def draw_circles_with_tooltips(
                 'boxstyle': 'round,pad=0.5',
                 'facecolor': (1.0, 1.0, 1.0, 0.9),
                 'linewidth': 0.5,
-                'zorder': 2,
+                'zorder': 3,
             },
         )
     virtual_file = StringIO()
@@ -128,11 +202,11 @@ def draw_circles_with_tooltips(
     tree, xmlid = ElementTree.XMLID(virtual_file.getvalue())
     tree.set('onload', 'init(evt)')
 
-    for index, circle in enumerate(circles):
+    for index, circle in enumerate(all_circles):
         element = xmlid[f'tooltip_{index:08d}']
         element.set('visibility', 'hidden')
 
-    for index, circle in enumerate(circles):
+    for index, circle in enumerate(all_circles):
         element = xmlid[f'patch_{index:08d}']
         element.set('onmouseover', f"ShowTooltip('tooltip_{index:08d}')")
         element.set('onmouseout', f"HideTooltip('tooltip_{index:08d}')")
@@ -160,14 +234,17 @@ def draw_circles_with_tooltips(
 if __name__ == '__main__':
     from math import sin, cos
     demo_data = []
-    for i in range(0, 1000, 2):
-        demo_data.append({
-            'x': (i / 1000) ** 2 * cos(i / 25),
-            'y': i**0.5 * sin(i / 25),
-            'text': f'{i}\n{i**0.5:.0f}',
-            'color': (1.0 * i / 1000, abs(cos(i / 25)), 1.0 - i / 1000),
-            'size': i / 20,
-        })
+    for j in range(0, 10):
+        demo_data_line = []
+        for i in range(0, 100, 2):
+            demo_data_line.append({
+                'x': ((i + j * 100) / 1000) ** 2 * cos((i + j * 100) / 25),
+                'y': (i + j * 100)**0.5 * sin((i + j * 100) / 25),
+                'text': f'{i}\n{i**0.5:.0f}\n{j}',
+                'color': (1.0 * (i + j * 100) / 1000, abs(cos((i + j * 100) / 25)), 1.0 - (i + j * 100) / 1000),
+                'size': (i + j * 100) / 20,
+            })
+        demo_data.append(demo_data_line)
     RGB_COLOR_MAP = {
         'red': (1, 0, 0),
         'blue': (0, 0, 1),
@@ -178,8 +255,8 @@ if __name__ == '__main__':
     draw_circles_with_tooltips(
         xlabel='X LABEL', ylabel='Y LABEL',
         title='Demo',
-        circles=demo_data,
-        color_legend=RGB_COLOR_MAP,
+        circle_lines=demo_data,
+        asset_color_map=RGB_COLOR_MAP,
         directory='result',
-        filename='plot_demo.svg'
+        filename='plot_demo'
     )
--- modules/portfolio.py
+++ modules/portfolio.py
@@ -4,10 +4,12 @@ import math
 import statistics
 
 
+# pylint: disable=too-many-instance-attributes
 class Portfolio:
     def __init__(self, weights):
         self.weights = weights
-        self.annual_gains = []
+        self.annual_gains = {}
+        self.annual_capital = {}
         self.stat_gain = -1
         self.stat_stdev = -1
         self.stat_cagr = -1
@@ -19,19 +21,21 @@ class Portfolio:
 
     def simulate(self, market_data):
         capital = 1
+        self.annual_capital[list(market_data.keys())[0] - 1] = 1
         for year in market_data.keys():
             new_capital = 0
             for ticker, weight in self.weights:
                 new_capital += capital * weight / 100 * market_data[year][ticker]
             if capital > 0:
-                self.annual_gains.append(new_capital / capital)
+                self.annual_gains[year] = new_capital / capital
             capital = new_capital
+            self.annual_capital[year] = new_capital
 
-        self.stat_gain = math.prod(self.annual_gains)
-        self.stat_stdev = statistics.stdev(self.annual_gains)
-        self.stat_cagr = self.stat_gain**(1 / len(self.annual_gains)) - 1
-        self.stat_var = sum((ann_gain - self.stat_cagr - 1) ** 2 for ann_gain in self.annual_gains)
-        self.stat_var /= (len(self.annual_gains) - 1)
+        self.stat_gain = math.prod(self.annual_gains.values())
+        self.stat_stdev = statistics.stdev(self.annual_gains.values())
+        self.stat_cagr = self.stat_gain**(1 / len(self.annual_gains.values())) - 1
+        self.stat_var = sum((ann_gain - self.stat_cagr - 1) ** 2 for ann_gain in self.annual_gains.values())
+        self.stat_var /= (len(self.annual_gains.values()) - 1)
         self.stat_sharpe = self.stat_cagr / self.stat_stdev
 
     def __repr__(self):
@@ -52,14 +56,8 @@ class Portfolio:
         ])
 
     def plot_tooltip(self):
-        weights_without_zeros = []
-        for ticker, weight in self.weights:
-            if weight == 0:
-                continue
-            weights_without_zeros.append(f'{ticker}: {weight}%')
-        str_weights = '\n'.join(weights_without_zeros)
         return '\n'.join([
-            f'{str_weights}',
+            f'{self.plot_title()}',
             '--- statistics ---',
             f'GAIN  : {self.stat_gain:.3f}',
             f'CAGR  : {self.stat_cagr*100:.2f}%',
@@ -67,6 +65,14 @@ class Portfolio:
             f'STDEV : {self.stat_stdev:.3f}',
             f'SHARP : {self.stat_sharpe:.3f}'
         ])
+
+    def plot_title(self):
+        weights_without_zeros = []
+        for ticker, weight in self.weights:
+            if weight == 0:
+                continue
+            weights_without_zeros.append(f'{ticker}: {weight}%')
+        return '\n'.join(weights_without_zeros)
 
     def plot_color(self, color_map):
         color = [0, 0, 0, 1]
