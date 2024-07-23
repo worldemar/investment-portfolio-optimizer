c3a362f91798efb98670c1142c9281c038b7290b
Vladimir Looze <woldemar@mimas.ru>
2024-07-23 21:51:57 +0300

    more tests for convex hull

--- modules/convex_hull_test.py
+++ modules/convex_hull_test.py
@@ -5,46 +5,6 @@ from scipy.spatial import ConvexHull
 import math
 import pytest
 
-def _compare_convex_hull_points_to_zero_layer(incremental_hull, scipy_hull):
-    def _str_point(p: Point):
-        return f'({p.x():.3f},{p.y():.3f})'
-    def _str_vertex(v: list[float]):
-        return f'({v[0]:.3f},{v[1]:.3f})'
-    ich_points = incremental_hull.hull_points()[0]
-    ch_points = scipy_hull.points[scipy_hull.vertices]
-    assert(len(ich_points) == len(ch_points))
-    ich_points = ' '.join(_str_point(i) for i in sorted(ich_points, key=lambda i: _str_point(i)))
-    ch_points = ' '.join(_str_vertex(i) for i in sorted(ch_points, key=lambda i: _str_vertex(i)))
-    assert(ich_points == ch_points)
-
-@pytest.mark.parametrize(
-    "name, points",
-    [
-        ['square', [[-1, 1], [1, 1], [1, -1], [-1, -1]]],
-        ['diamond', [[-1, 0], [0, 1], [1, 0], [0, -1]]],
-     ],
-)
-def test_IncrementalConvexHull_add_one_point_to_layer(name, points):
-    class PointMock(Point):
-        def __init__(self, x, y):
-            self._x = x
-            self._y = y
-        def x(self):
-            return self._x
-        def y(self):
-            return self._y
-    N = 1024
-    for i in range(N):
-        ich = LazyMultilayerConvexHull(points=[PointMock(x,y) for x,y in points], max_dirty_points=100, layers=1)
-        ch = ConvexHull(points=points, incremental=True)
-        _compare_convex_hull_points_to_zero_layer(ich, ch)
-
-        x = 0.75 * math.cos(i * 2 * math.pi / N)
-        y = 0.75 * math.sin(i * 2 * math.pi / N)
-        ich.add_point(PointMock(x, y))
-        ch.add_points([[x, y]])
-        _compare_convex_hull_points_to_zero_layer(ich, ch)
-
 @pytest.mark.parametrize(
     "points, layers",
     [
@@ -52,9 +12,122 @@ def test_IncrementalConvexHull_add_one_point_to_layer(name, points):
             [[-1, 1], [1, 1], [1, -1], [-1, -1]],
             [
                 [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+            ]
+        ],
+        [
+            [[-1, 1], [1, 1], [1, -1], [-1, -1]],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
                 [],
             ]
-        ]
+        ],
+        [
+            [[-1, 1], [1, 1], [1, -1], [-1, -1]],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [], [],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5]
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5], [0.5, -0.5]
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5]
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [[-0.5, -0.5], [0.5, -0.5], [0.5, 0.5]],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5]
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [[-0.5, -0.5], [0.5, -0.5], [0.5, 0.5]],
+                [],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5], [-0.5, 0.5]
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [[-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5]],
+                [],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5], [-0.5, 0.5],
+                [0.3, 0.3]
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [[-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5]],
+                [],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5], [-0.5, 0.5],
+                [0.3, 0.3], [-0.3, -0.3],
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [[-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5]],
+                [],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5], [-0.5, 0.5],
+                [0.3, 0.3], [-0.3, -0.3], [0.3, -0.3],
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [[-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5]],
+                [[-0.3, -0.3], [0.3, -0.3], [0.3, 0.3],],
+            ]
+        ],
+        [
+            [
+                [-1, 1], [1, 1], [1, -1], [-1, -1],
+                [0.5, 0.5], [0.5, -0.5], [-0.5, -0.5], [-0.5, 0.5],
+                [0.3, 0.3], [-0.3, -0.3], [0.3, -0.3], [-0.3, 0.3],
+            ],
+            [
+                [[-1, -1], [-1, 1], [1, -1], [1, 1]],
+                [[-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5]],
+                [[-0.3, -0.3], [-0.3, 0.3], [0.3, -0.3], [0.3, 0.3],],
+            ]
+        ],
     ]
 )
 def test_IncrementalConvexHull_add_one_point(points, layers):
