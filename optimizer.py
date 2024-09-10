7f039f8218dc7f04f5abe43ce90127901ffb80f4
Vladimir Looze <woldemar@mimas.ru>
2024-09-10 18:59:42 +0300

    logging info

--- optimizer.py
+++ optimizer.py
@@ -20,7 +20,7 @@ from static_portfolios import STATIC_PORTFOLIOS
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s :: %(levelname)s :: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
     )
