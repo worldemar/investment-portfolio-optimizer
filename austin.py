91d564e82313761b26caa83ec57e7325294e3c2d
Vladimir Looze <woldemar@mimas.ru>
2024-09-27 15:50:34 +0300

    austin

--- austin.py
+++ austin.py
@@ -1,27 +1,91 @@
 #!/usr/bin/env python3
 
 import random
-from modules.convex_hull import LazyMultilayerConvexHull, ConvexHullPoint
+import time
+import os
+import psutil
+import importlib
+from modules.convex_hull import LazyMultilayerConvexHull, ConvexHullPoint, batched_multilayer_convex_hull, multilayer_convex_hull
 
 class PointMock(ConvexHullPoint):
-    def __init__(self, x, y):
-        self._x = x
-        self._y = y
+    def __new__(cls, x, y, payload):
+        return super().__new__(cls, (x, y))
 
-    def x(self):
-        return self._x
+    def __init__(self, x, y, payload):
+        self.payload = payload
 
-    def y(self):
-        return self._y
+def main():
+    pyhull_convex_hull = importlib.import_module('pyhull.convex_hull').ConvexHull
 
-    def __lt__(self, other):
-        return [self._x, self._y] < [other._x, other._y]
+    timestamp_points_start = time.time()
+    N = 1000*1000
+    random_points = [PointMock(random.random(), random.random(), n) for n in range(N)]
+    timestamp_points_end = time.time()
+    print(f'generating {N} points :: {timestamp_points_end - timestamp_points_start:.2f}s ')
+    base_used_ram = psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024
+    print(f'used ram: {base_used_ram:.2f} MB')
 
-    def __repr__(self):
-        return f'[{self._x}, {self._y}]'
+    timestamp_qhull_start = time.time()
+    qhull = pyhull_convex_hull(random_points)
+    qhull_vertexes = set(vertex for hull_vertex in qhull.vertices for vertex in hull_vertex)
+    qhull_points = list(random_points[vertex] for vertex in qhull_vertexes)
+    timestamp_qhull_end = time.time()
+    print(f'qhull 3x :: {(timestamp_qhull_end - timestamp_qhull_start)*3:.2f}s ')
+    qhull_points.sort()
 
+    LAYERS = 3
+    BATCH=1000000
 
-lmch = LazyMultilayerConvexHull(max_dirty_points=1000, layers=3)
-for i in range(10000):
-    lmch(PointMock(random.random(), random.random()))
-hull = lmch(None)
+    random_points = (PointMock(random.random(), random.random(), n) for n in range(N))
+    base_used_ram = psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024
+    timestamp_mhull_start = time.time()
+    mhull_points, max_used_ram = multilayer_convex_hull(random_points, layers=LAYERS)
+    timestamp_mhull_end = time.time()
+    print(f'mhull :: {timestamp_mhull_end - timestamp_mhull_start:.2f}s {max_used_ram-base_used_ram:.2f} MB')
+    mhull_points.sort()
+
+    random_points = (PointMock(random.random(), random.random(), n) for n in range(N))
+    base_used_ram = psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024
+    timestamp_bhull_start = time.time()
+    bhull_points, max_used_ram = batched_multilayer_convex_hull(random_points, batch_size=BATCH, layers=LAYERS)
+    timestamp_bhull_end = time.time()
+    print(f'bhull :: {timestamp_bhull_end - timestamp_bhull_start:.2f}s {max_used_ram-base_used_ram:.2f} MB')
+    bhull_points.sort()
+
+    # assert bhull_points == mhull_points
+
+    random_points = (PointMock(random.random(), random.random(), n) for n in range(N))
+    base_used_ram = psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024
+    lmch = LazyMultilayerConvexHull(max_dirty_points=BATCH, layers=LAYERS)
+    timestamp_points_lmch_start = time.time()
+    for i in random_points:
+        lmch(i)
+    lmch_hull_points = lmch.points()
+    timestamp_points_lmch_end = time.time()
+    print(f'lmch :: {timestamp_points_lmch_end - timestamp_points_lmch_start:.2f}s {lmch.max_used_ram-base_used_ram:.2f} MB')
+    lmch_hull_points.sort()
+
+    # assert lmch_hull_points == bhull_points
+
+def main2():
+    pyhull_convex_hull = importlib.import_module('pyhull.convex_hull').ConvexHull
+    REPEATS=10
+    for N in range(10,20):
+        random_points = [PointMock(random.random(), random.random(), n) for n in range(2**N)]
+        unsorted_start = time.time()
+        for r in range(REPEATS):
+            unsorted_hull = pyhull_convex_hull(random_points)
+        unsorted_end = time.time()
+        unsorted_batch_start = time.time()
+        for r in range(REPEATS):
+            sorted_hull = batched_multilayer_convex_hull(random_points, batch_size=100000)
+        unsorted_batch_end = time.time()
+        random_points.sort()
+        sorted_start = time.time()
+        for r in range(REPEATS):
+            sorted_hull = pyhull_convex_hull(random_points)
+        sorted_end = time.time()
+        print(f'N={N} :: unsorted :: {(unsorted_end - unsorted_start)/REPEATS:.2f}s sorted :: {(sorted_end - sorted_start)/REPEATS:.2f}s :: batch :: {(unsorted_batch_end - unsorted_batch_start)/REPEATS:.2f}s')
+
+if __name__ == '__main__':
+    main2()
