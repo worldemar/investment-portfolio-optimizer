8ba1cfba4e98b50817fbff0153c5c12a9a93998a
Vladimir Looze <woldemar@mimas.ru>
2024-10-06 11:40:49 +0300

    use indexed types in static portfolios

--- config/static_portfolios.py
+++ config/static_portfolios.py
@@ -1,37 +1,37 @@
-from modules.data_types import Portfolio
+from modules.data_types import UserPortfolio
 
 STATIC_PORTFOLIOS = [
-    Portfolio(plot_always=True, plot_marker='X', weights={
+    UserPortfolio({
         'Акции РФ': 60,
         'Серебро': 20,
         'Золото': 20,
     }),
-    Portfolio(plot_always=True, plot_marker='X', weights={
+    UserPortfolio({
         'Акции РФ': 60,
         'Золото': 40,
     }),
-    Portfolio(plot_always=True, plot_marker='X', weights={
+    UserPortfolio({
         'Акции РФ': 33.334,
         'Золото': 33.333,
         'Гос. облигации РФ (до 3 лет)': 33.333,
     }),
-    Portfolio(plot_always=True, plot_marker='X', weights={
+    UserPortfolio({
         'Акции РФ': 33.334,
         'Золото': 33.333,
         'Гос. облигации РФ (ОФЗ-ИН)': 33.333,
     }),
-    Portfolio(plot_always=True, plot_marker='X', weights={
+    UserPortfolio({
         'Жильё в РФ': 33.334,
         'Гос. облигации РФ (до 3 лет)': 33.333,
         'Гос. облигации РФ (ОФЗ-ИН)': 33.333,
     }),
-    Portfolio(plot_always=True, plot_marker='X', weights={
+    UserPortfolio({
         'Жильё в РФ': 25,
         'Акции РФ': 25,
         'Золото': 25,
         'Гос. облигации РФ (до 3 лет)': 25,
     }),
-    Portfolio(plot_always=True, plot_marker='X', weights={
+    UserPortfolio({
         'Жильё в РФ': 25,
         'Акции РФ': 25,
         'Золото': 12.5,
