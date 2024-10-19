e6106f07ed837bc5bf40bbf47071ed995dd9baf0
Vladimir Looze <woldemar@mimas.ru>
2024-10-19 17:05:24 +0300

    split allocations per core

--- modules/data_source.py
+++ modules/data_source.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import csv
 import time
 import config.asset_colors
@@ -15,6 +16,7 @@ import collections
 from config.config import CHUNK_SIZE
 import config
 import pickle
+import queue
 
 def allocation_simulate(asset_allocation, assets, asset_revenue_per_year):
     portfolio = Portfolio(assets=assets, weights=asset_allocation)
@@ -25,6 +27,24 @@ def allocation_simulate_serialize(asset_allocation, assets, asset_revenue_per_ye
     portfolio = Portfolio(assets=assets, weights=asset_allocation)
     portfolio.simulate(asset_revenue_per_year)
     return portfolio.serialize()
+
+def allocation_simulate_serialize_slice_to_sink(slice_idx, slice_size, assets, percentage_step, asset_revenue_per_year, sink):
+    portfolios_sent = 0
+    with concurrent.futures.ThreadPoolExecutor() as thread_executor:
+        possible_allocations_gen = all_possible_allocations(assets, percentage_step)
+        slice_gen = itertools.islice(possible_allocations_gen, slice_idx * slice_size, (slice_idx + 1) * slice_size)
+        simulate_func = partial(allocation_simulate_serialize, assets=assets, asset_revenue_per_year=asset_revenue_per_year)
+        simulated_portfolios = map(simulate_func, slice_gen)
+        send_task = None
+        for batch in itertools.batched(simulated_portfolios, CHUNK_SIZE):
+            if send_task is not None:
+                send_task.result()
+            send_task = thread_executor.submit(sink.send_bytes, b''.join(batch))
+            portfolios_sent += len(batch)
+        if send_task is not None:
+            send_task.result()
+    return portfolios_sent
+
 
 def all_possible_allocations(assets: list, step: int):
     def _allocations_recursive(
@@ -53,34 +73,71 @@ def all_possible_allocations(assets: list, step: int):
         allocation = [0] * len(assets),
         allocation_sum = 0)
 
+def all_possible_allocations_to_queue(assets: list, step: int, sink: queue.Queue):
+    for possible_allocation in all_possible_allocations(assets, step):
+        sink.put(possible_allocation)
+    return
+
+def queue_batched(future, source: queue.Queue, batch_size: int):
+    chunk = []
+    while not source.empty() and not future.done():
+        chunk.append(source.get())
+        if len(chunk) >= batch_size:
+            yield chunk
+            chunk = []
+    if len(chunk) > 0:
+        yield chunk
+
 def simulated_q(
         assets: list = None,
         percentage_step: int = None,
         asset_revenue_per_year: dict[str, dict[str, float]] = None,
         sink: multiprocessing.connection.Connection = None):
     logger = logging.getLogger(__name__)
-    process_pool = multiprocessing.Pool()
+    process_pool = concurrent.futures.ProcessPoolExecutor()
     thread_pool = concurrent.futures.ThreadPoolExecutor()
 
-    # allocation_limit = 10*1000*1000
-    # allocation_limit = 1000*1000
-    # allocation_limit = 200*1000
-    # allocation_limit = 10*1000
+    possible_allocations_gen = all_possible_allocations(assets, percentage_step)
+    total_possible_allocations = sum(1 for _ in possible_allocations_gen)
 
     time_start = time.time()
-
-    # possible_allocations_gen = itertools.islice(all_possible_allocations(assets, percentage_step), allocation_limit)
-    possible_allocations_gen = all_possible_allocations(assets, percentage_step)
-    simulate_func = partial(allocation_simulate_serialize, assets=assets, asset_revenue_per_year=asset_revenue_per_year)
-    total_portfolios = 0
-    for possible_allocation_batch in itertools.batched(possible_allocations_gen, CHUNK_SIZE):
-        simulated_portfolios_batch = list(process_pool.map(simulate_func, possible_allocation_batch))
-        total_portfolios += len(simulated_portfolios_batch)
-        bytes = b''.join(simulated_portfolios_batch)
-        sink.send_bytes(bytes)
-    thread_pool.shutdown()
+    allocations_per_core = total_possible_allocations // os.cpu_count() + 1
+    slice_sender = partial(
+        allocation_simulate_serialize_slice_to_sink,
+        slice_size=allocations_per_core,
+        assets=assets,
+        percentage_step=percentage_step,
+        asset_revenue_per_year=asset_revenue_per_year,
+        sink=sink)
+    portfolios_sent_per_core = process_pool.map(slice_sender, range(0, os.cpu_count()))
+    process_pool.shutdown()
+    time_end = time.time()
+    logger.info(f'Simulated {sum(portfolios_sent_per_core)} portfolios, rate: {int(total_possible_allocations / (time_end - time_start))}/s')
     sink.send(data_types.DataStreamFinished())
-    logger.info(f'Simulated {total_portfolios} portfolios, rate: {int(total_portfolios / (time.time() - time_start))}/s')
+
+    # # allocation_limit = 10*1000*1000
+    # # allocation_limit = 1000*1000
+    # # allocation_limit = 200*1000
+    # # allocation_limit = 10*1000
+
+    # time_start = time.time()
+
+    # # possible_allocations_gen = itertools.islice(all_possible_allocations(assets, percentage_step), allocation_limit)
+    # possible_allocations_gen = all_possible_allocations(assets, percentage_step)
+    # simulate_func = partial(allocation_simulate_serialize, assets=assets, asset_revenue_per_year=asset_revenue_per_year)
+    # total_portfolios = 0
+    # send_request = None
+    # for possible_allocation_batch in queue_batched(allocations_future, allocations_queue, CHUNK_SIZE):
+    #     simulated_portfolios_batch = process_pool.map(simulate_func, possible_allocation_batch)
+    #     total_portfolios += len(simulated_portfolios_batch)
+    #     bytes = b''.join(simulated_portfolios_batch)
+    #     # if send_request is not None:
+    #         # send_request.result()
+    #     # send_request = thread_pool.submit(sink.send_bytes, bytes)
+    # # send_request.result()
+    # thread_pool.shutdown()
+    # sink.send(data_types.DataStreamFinished())
+    # logger.info(f'Simulated {total_portfolios} portfolios, rate: {int(total_portfolios / (time.time() - time_start))}/s')
 
 
 def read_capitalgain_csv_data(filename):
