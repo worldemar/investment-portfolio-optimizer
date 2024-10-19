11fd9995900976be6011ed05f709928d144c4997
Vladimir Looze <woldemar@mimas.ru>
2024-10-19 23:45:01 +0300

    fix wrong rate calculation

--- modules/simulator.py
+++ modules/simulator.py
@@ -17,10 +17,10 @@ def simulator_process_func(
         chunk_size: int = 1):
     logger = logging.getLogger(__name__)
     possible_allocations_gen = data_source.all_possible_allocations(len(assets), percentage_step)
-    total_possible_allocations = sum(1 for _ in possible_allocations_gen)
+    possible_allocations = sum(1 for _ in possible_allocations_gen)
+    time_start = time.time()
     with ProcessPoolExecutor() as process_pool:
-        time_start = time.time()
-        allocations_per_core = total_possible_allocations // os.cpu_count() + 1
+        allocations_per_core = possible_allocations // os.cpu_count() + 1
         slice_sender = partial(
             data_source.allocation_slice_simulate_and_feed_to_sink,
             slice_size=allocations_per_core,
@@ -30,6 +30,6 @@ def simulator_process_func(
             sink=sink,
             chunk_size=chunk_size)
         portfolios_sent_per_core = process_pool.map(slice_sender, range(0, os.cpu_count()))
-        time_end = time.time()
-    logger.info(f'Simulated {sum(portfolios_sent_per_core)} portfolios, rate: {int(total_possible_allocations / (time_end - time_start))}/s')
+    time_end = time.time()
+    logger.info(f'Simulated {sum(portfolios_sent_per_core)} portfolios, rate: {possible_allocations // int(time_end - time_start) // 1000}k/s')
     sink.send(data_source.DataStreamFinished())
