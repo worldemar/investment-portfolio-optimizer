186dc21e603e8da33c81dc943da9c430f8189b90
Vladimir Looze <woldemar@mimas.ru>
2024-10-21 00:24:16 +0300

    rename revenue to gain

--- modules/data_source.py
+++ modules/data_source.py
@@ -47,7 +47,7 @@ def all_possible_allocations(assets_n: int, step: int):
 # pylint: disable=too-many-positional-arguments
 def allocation_slice_simulate_and_feed_to_sink(
         slice_idx, slice_size,
-        assets, percentage_step, asset_revenue_per_year,
+        assets, percentage_step, asset_gain_per_year,
         sink, chunk_size):
     portfolios_sent = 0
     with ThreadPoolExecutor() as thread_executor:
@@ -57,7 +57,7 @@ def allocation_slice_simulate_and_feed_to_sink(
             partial(Portfolio, assets=assets),
             gen_slice_allocations)
         gen_simulateds = map(
-            partial(Portfolio.simulated, asset_revenue_per_year=asset_revenue_per_year),
+            partial(Portfolio.simulated, asset_gain_per_year=asset_gain_per_year),
             gen_portfolios)
         gen_serializations = map(Portfolio.serialize, gen_simulateds)
         send_task = None
@@ -72,7 +72,7 @@ def allocation_slice_simulate_and_feed_to_sink(
 
 
 def read_capitalgain_csv_data(filename):
-    yearly_revenue_multiplier = {}  # year, ticker = cash multiplier
+    yearly_gain = {}  # year, ticker = cash multiplier
     # read csv values from tickers.csv
     rows = []
     with open(filename, "r", encoding="utf-8") as csv_file:
@@ -80,12 +80,12 @@ def read_capitalgain_csv_data(filename):
         rows = list(csv_reader)
     assets = rows[0][1:]
     for row in rows[1:]:
-        if row[0] not in yearly_revenue_multiplier:
-            yearly_revenue_multiplier[int(row[0])] = [0] * len(assets)
+        if row[0] not in yearly_gain:
+            yearly_gain[int(row[0])] = [0] * len(assets)
         for i in range(1, len(row)):
-            yearly_revenue_multiplier[int(row[0])][i - 1] = \
+            yearly_gain[int(row[0])][i - 1] = \
                 float(row[i].replace('%', '')) / 100 + 1
-    return assets, yearly_revenue_multiplier
+    return assets, yearly_gain
 
 
 # pylint: disable=too-few-public-methods
