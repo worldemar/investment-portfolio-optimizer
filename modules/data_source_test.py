7151ebb1fe578a5fdb824eb2d39bef712c590032
Vladimir Looze <woldemar@mimas.ru>
2024-10-20 03:17:33 +0300

    do not measure performance in unit tests

--- modules/data_source_test.py
+++ modules/data_source_test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import time
 import itertools
 import pytest
 from modules import data_source
@@ -19,23 +18,14 @@ _divizors_of_100 = [1, 2, 4, 5, 10, 20, 25, 50, 100]
     )
 )
 def test_all_possible_allocations(assets_n: int, step: int):
-    time_e1 = time.time()
     expected_allocations_gen = _expected_allocations(assets_n, step)
     expected_allocations = list(tuple(a) for a in expected_allocations_gen)
-    time_e2 = time.time()
     expected_allocations.sort()
-
-    time_a1 = time.time()
     test_allocations_gen = data_source.all_possible_allocations(assets_n, step)
     test_allocations = list(tuple(a) for a in test_allocations_gen)
-    time_a2 = time.time()
     test_allocations.sort()
-
     # must be strictly equivalent to filtered product
     assert test_allocations == expected_allocations
-
-    # should never be slower than filtered product
-    assert time_e2 - time_e1 >= time_a2 - time_a1
 
 
 def _expected_allocations(assets_n: int, step: int):
