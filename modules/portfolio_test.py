0786d2124e3b5a3b2736e016c138104b963415ea
Vladimir Looze <woldemar@mimas.ru>
2024-10-21 21:14:37 +0300

    use dictionary for portfolio stats

--- modules/portfolio_test.py
+++ modules/portfolio_test.py
@@ -9,19 +9,24 @@ def test_portfolio_serialize():
         assets=['AAPL', 'MSFT', 'GOOG'],
         weights=[10, 40, 50],
     )
-    portfolio.stat_gain = 0.1
-    portfolio.stat_stdev = 0.2
-    portfolio.stat_cagr = 0.3
-    portfolio.stat_var = 0.4
-    portfolio.stat_sharpe = 0.5
+    portfolio.stat[Portfolio.STAT_GAIN] = 0.1
+    portfolio.stat[Portfolio.STAT_STDDEV] = 0.2
+    portfolio.stat[Portfolio.STAT_CAGR_PERCENT] = 0.3
+    portfolio.stat[Portfolio.STAT_VARIANCE] = 0.4
+    portfolio.stat[Portfolio.STAT_SHARPE] = 0.5
 
     serialized = portfolio.serialize()
     deserialized = Portfolio.deserialize(serialized, assets=portfolio.assets)
-    assert portfolio.stat_gain - deserialized.stat_gain < epsilon
-    assert portfolio.stat_stdev - deserialized.stat_stdev < epsilon
-    assert portfolio.stat_cagr - deserialized.stat_cagr < epsilon
-    assert portfolio.stat_var - deserialized.stat_var < epsilon
-    assert portfolio.stat_sharpe - deserialized.stat_sharpe < epsilon
+    assert portfolio.stat[Portfolio.STAT_GAIN] - \
+        deserialized.stat[Portfolio.STAT_GAIN] < epsilon
+    assert portfolio.stat[Portfolio.STAT_STDDEV] - \
+        deserialized.stat[Portfolio.STAT_STDDEV] < epsilon
+    assert portfolio.stat[Portfolio.STAT_CAGR_PERCENT] - \
+        deserialized.stat[Portfolio.STAT_CAGR_PERCENT] < epsilon
+    assert portfolio.stat[Portfolio.STAT_VARIANCE] - \
+        deserialized.stat[Portfolio.STAT_VARIANCE] < epsilon
+    assert portfolio.stat[Portfolio.STAT_SHARPE] - \
+        deserialized.stat[Portfolio.STAT_SHARPE] < epsilon
     assert portfolio.weights == deserialized.weights
 
 
@@ -33,19 +38,24 @@ def test_portfolio_serialize_batch():
             assets=['AAPL', 'MSFT', 'GOOG'],
             weights=[100 - i, i, 0],
         )
-        portfolio.stat_gain = 0.1 * i
-        portfolio.stat_stdev = 0.2 * i
-        portfolio.stat_cagr = 0.3 * i
-        portfolio.stat_var = 0.4 * i
-        portfolio.stat_sharpe = 0.5 * i
+        portfolio.stat[Portfolio.STAT_GAIN] = 0.1 * i
+        portfolio.stat[Portfolio.STAT_STDDEV] = 0.2 * i
+        portfolio.stat[Portfolio.STAT_CAGR_PERCENT] = 0.3 * i
+        portfolio.stat[Portfolio.STAT_VARIANCE] = 0.4 * i
+        portfolio.stat[Portfolio.STAT_SHARPE] = 0.5 * i
         portfolios.append(portfolio)
 
     serialized = b''.join(p.serialize() for p in portfolios)
-    deserialized_batch = list(Portfolio.deserialize_iter(serialized, assets=portfolio.assets))
+    deserialized = list(Portfolio.deserialize_iter(serialized, assets=portfolio.assets))
     for i in range(100):
-        assert portfolios[i].stat_gain - deserialized_batch[i].stat_gain < epsilon
-        assert portfolios[i].stat_stdev - deserialized_batch[i].stat_stdev < epsilon
-        assert portfolios[i].stat_cagr - deserialized_batch[i].stat_cagr < epsilon
-        assert portfolios[i].stat_var - deserialized_batch[i].stat_var < epsilon
-        assert portfolios[i].stat_sharpe - deserialized_batch[i].stat_sharpe < epsilon
-        assert portfolios[i].weights == deserialized_batch[i].weights
+        assert portfolios[i].stat[Portfolio.STAT_GAIN] - \
+            deserialized[i].stat[Portfolio.STAT_GAIN] < epsilon
+        assert portfolios[i].stat[Portfolio.STAT_STDDEV] - \
+            deserialized[i].stat[Portfolio.STAT_STDDEV] < epsilon
+        assert portfolios[i].stat[Portfolio.STAT_CAGR_PERCENT] - \
+            deserialized[i].stat[Portfolio.STAT_CAGR_PERCENT] < epsilon
+        assert portfolios[i].stat[Portfolio.STAT_VARIANCE] - \
+            deserialized[i].stat[Portfolio.STAT_VARIANCE] < epsilon
+        assert portfolios[i].stat[Portfolio.STAT_SHARPE] - \
+            deserialized[i].stat[Portfolio.STAT_SHARPE] < epsilon
+        assert portfolios[i].weights == deserialized[i].weights
